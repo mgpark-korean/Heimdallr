3e16b3f6c7e8ae38d1e93b62f6982bde37549426
mgpark <mgpark@enliple.com>
2021-04-26 18:53:39 +0900

    빌드 환경 분리는 위한 파일 삭제

--- webpack/dev.js
+++
@@ -1,9 +0,0 @@
-const devServerConfig = require('./config/devServerConfig');
-
-export default {
-  watch: true,
-  watchOptions: {
-    ignore: /node_modules/
-  },
-  devServer: devServerConfig,
-}
